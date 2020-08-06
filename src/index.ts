49895c136c3e848b7464431bb5c94332663ad720
Theo Paris <theo@throw-out-error.dev>
2020-08-06 16:07:21 -0700

    cleaned it up

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@ import fs from "fs";
 const db = JSON.parse(fs.readFileSync("./videos.json", "utf-8"));
 
 const app = express();
-app.use(express.static("./css"));
-app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/../css"));
+app.set("views", __dirname + "/../views");
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-    res.render('index', {ids: db});
+    res.render("index", { ids: db });
 });
 app.listen(process.env.PORT || 8080);
